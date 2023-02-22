cbd24ac97ff340525851ea8ebc703122ba2746f7
rabiakuran <rabiakuran34@gmail.com>
2023-02-22 23:35:31 +0300

    veriler çekilde

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {
   SafeAreaView,
   Text,
@@ -13,20 +13,64 @@ import {
   ActivityIndicator,
   Button,
 } from 'react-native';
+import axios from 'axios'; // paketi sayfamıza ekliyoruz
+import {Grid} from '@mui/material';
 
+type Movie = {
+  id: string;
+  adSoyad: string;
+  gorev: string;
+};
 
-const HomePage = (props: { navigation: any; }) => {
+const HomePage = (props: {navigation: any}) => {
   const {navigation} = props;
+  const [isLoading, setLoading] = useState(false);
+  const [datas, setDatas] = useState<Movie[]>();
+  // define state
+  class App extends Component {
+    state = {
+      emp: [],
+    };
+
+    constructor() {
+      super(props);
+    }
+  }
+
+  // GET Request Using Axios HTTP // get ile veri çekme
+  const getEmployee = async () => {
+    await axios
+      .get('http://10.0.2.2:8080/api/employees/getall')
+      .then(res => {
+        const data = res.data;
+        //this.setState({ emps });
+        console.log('data geliyor');
+        console.log(res.data);
+        setDatas(res.data.data);
+      })
+      .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    getEmployee();
+  }, []);
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Profile"
-        onPress={() => navigation.navigate('Profil',{
-            username: 'Rabia',
-            password: '242141',
-          })}
-      />
+    <View style={{flex: 1, padding: 24}}>
+      <Text>ÇALIŞANLAR</Text>
+  
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={datas}
+          keyExtractor={({id}) => id}
+          renderItem={({item}) => (
+            <Text>
+              {item.adSoyad} - {item.gorev}
+            </Text>
+          )}
+        />
+      )}
     </View>
   );
 };
